bcf474f867622efcc494d17f71b7daf008f3c6c3
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-07-13 21:31:11 -0700

    Ouch. Init is hard.

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,29 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = "Fido", breed ="Mastiff"):
+        self.set_name(name)
+        self.set_breed(breed)
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, value):
+        # Define a name property for your Dog class. The name must be of type str and between 1 and 25 characters.
+        #If the name is invalid, the setter method should print() "Name must be string between 1 and 25 characters."
+        if not isinstance(value, str) or len(value) <= 0 or len(value) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, value):
+        if value not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "John", job ="Admin"):
+        self.set_name(name)
+        self.set_job(job)
+    
+    def set_name(self, name):
+        if not isinstance(name, str) or len(name) <= 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name.title()
+
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    def get_name(self):
+        return self._name
+    
+    def get_job(self):
+        return self._job
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
